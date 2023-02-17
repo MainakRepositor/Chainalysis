a4901297d2e0b0344bb928e28988cd0934420f8c
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-02-17 09:47:15 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -73,10 +73,3 @@ st.write(
     """
 )
 
-c1, c2, c3 = st.columns(3)
-with c1:
-    st.info('**Data Analyst: [@AliTslm](https://twitter.com/AliTslm)**', icon="💡")
-with c2:
-    st.info('**GitHub: [@alitaslimi](https://github.com/alitaslimi)**', icon="💻")
-with c3:
-    st.info('**Data: [Flipside Crypto](https://flipsidecrypto.xyz)**', icon="🧠")
